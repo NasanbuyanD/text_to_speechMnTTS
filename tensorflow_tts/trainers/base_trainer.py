9381cd711f6b84703009f77e59184de75c4ff4ff
D. Nasanbuyan <114938980+NasanbuyanD@users.noreply.github.com>
2024-03-01 15:01:16 +0800

    Update base_trainer.py

--- tensorflow_tts/trainers/base_trainer.py
+++ tensorflow_tts/trainers/base_trainer.py
@@ -378,25 +378,25 @@ class GanBasedTrainer(BasedTrainer):
 
 
     def _accumulate_gradients(self, model, optimizer, batch, loss_fn):
-    per_replica_losses = 0.0
-    for i in tf.range(self.config["gradient_accumulation_steps"]):
-        reduced_batch = {
-            k: v[
-                i
-                * self.config["batch_size"] : (i + 1)
-                * self.config["batch_size"]
-            ]
-            for k, v in batch.items()
-        }
-        reduced_batch_losses = loss_fn(reduced_batch)
-        per_replica_losses += reduced_batch_losses
-
-    gradients = model.gradient_accumulator.gradients
-    optimizer.apply_gradients(
-        zip(gradients, model.trainable_variables)
-    )
-    model.gradient_accumulator.reset()
-    return per_replica_losses
+        per_replica_losses = 0.0
+        for i in tf.range(self.config["gradient_accumulation_steps"]):
+            reduced_batch = {
+                k: v[
+                    i
+                    * self.config["batch_size"] : (i + 1)
+                    * self.config["batch_size"]
+                ]
+                for k, v in batch.items()
+            }
+            reduced_batch_losses = loss_fn(reduced_batch)
+            per_replica_losses += reduced_batch_losses
+    
+        gradients = model.gradient_accumulator.gradients
+        optimizer.apply_gradients(
+            zip(gradients, model.trainable_variables)
+        )
+        model.gradient_accumulator.reset()
+        return per_replica_losses
 
     def _one_step_forward_per_replica(self, batch):
         per_replica_gen_losses = 0.0
